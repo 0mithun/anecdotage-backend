cb7dc0b895758a2717b09a970e96044f5a2f1f29
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-01-20 16:57:07 +0600

    Add comment for test

--- database/migrations/2020_12_02_070407_create_threads_table.php
+++ database/migrations/2020_12_02_070407_create_threads_table.php
@@ -16,41 +16,41 @@ class CreateThreadsTable extends Migration
         Schema::create('threads', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('slug')->unique()->nullable();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('channel_id')->default(1);
-            $table->string('title', 255);
-            $table->text('body');
+            // $table->unsignedBigInteger('user_id');
+            // $table->unsignedBigInteger('channel_id')->nullable();
+            // $table->string('title', 255);
+            // $table->text('body');
 
-            $table->text('summary')->nullable();
-            $table->text('source')->nullable();
-            $table->string('main_subject')->nullable();
-            $table->string('image_path')->nullable();
-            $table->string('image_path_pixel_color', 50)->nullable();
-            $table->text('image_description')->nullable();
-            $table->string('temp_image_url')->nullable();
-            $table->string('temp_image_description')->nullable();
-            $table->boolean('image_saved')->default(0);
+            // $table->text('summary')->nullable();
+            // $table->text('source')->nullable();
+            // $table->string('main_subject')->nullable();
+            // $table->string('image_path')->nullable();
+            // $table->string('image_path_pixel_color', 50)->nullable();
+            // $table->text('image_description')->nullable();
+            // $table->string('temp_image_url')->nullable();
+            // $table->string('temp_image_description')->nullable();
+            // $table->boolean('image_saved')->default(0);
 
-            $table->string('cno')->nullable();
-            $table->integer('age_restriction')->default(0);
-            $table->boolean('anonymous')->default(0);
+            // $table->string('cno')->nullable();
+            // $table->integer('age_restriction')->default(0);
+            // $table->boolean('anonymous')->default(0);
 
-            $table->string('formatted_address')->nullable();
-            $table->point('location')->nullable();
+            // $table->string('formatted_address')->nullable();
+            // $table->point('location')->nullable();
 
-            $table->boolean('is_published')->default(1);
-            $table->bigInteger('visits')->default(0);
-            $table->bigInteger('favorite_count')->default(0);
-            $table->bigInteger('like_count')->default(0);
-            $table->bigInteger('dislike_count')->default(0);
-            //New Items
-            $table->boolean('famous')->default(0);
-            $table->text('slide_body')->nullable();
-            $table->string('slide_image_pos')->nullable();
-            $table->string('slide_color_bg')->nullable();
-            $table->string('slide_color_0')->nullable();
-            $table->string('slide_color_1')->nullable();
-            $table->string('slide_color_2')->nullable();
+            // $table->boolean('is_published')->default(1);
+            // $table->bigInteger('visits')->default(0);
+            // $table->bigInteger('favorite_count')->default(0);
+            // $table->bigInteger('like_count')->default(0);
+            // $table->bigInteger('dislike_count')->default(0);
+            // //New Items
+            // $table->boolean('famous')->default(0);
+            // $table->text('slide_body')->nullable();
+            // $table->string('slide_image_pos')->nullable();
+            // $table->string('slide_color_bg')->nullable();
+            // $table->string('slide_color_0')->nullable();
+            // $table->string('slide_color_1')->nullable();
+            // $table->string('slide_color_2')->nullable();
 
             $table->timestamps();
         });
