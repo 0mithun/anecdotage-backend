a7a6b549bfdaac8f70a1ccb95b019431b961bcd9
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-01-26 20:19:19 +0600

    Remove anomyous route from web route

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,6 @@
 use Illuminate\Http\Request;
 
 
-Route::get('/', function (Request $request) {
-    // $query = (new \App\Models\Design)->newQuery();
-    // if ($request->q) {
-    //     $query->orWhere(function($q) use ($request){
-    //         $q->where('title', 'LIKE', '%'.$request->q.'%')
-    //             ->orWhere('description', 'LIKE', '%'.$request->q.'%');
-    //     });
-    // }
-    // $query->has('comments');
-    // $q->has('team');
-    // $q->withCount('likes')
-    //     ->orderByDesc('likes_count');
-    
-    
-    // get by tag
-    //$q->withAllTags('adobe-photoshop');
-    // dd($query->get());
-
-
-
-
-    return view('welcome');
-});
+// Route::get('/', function (Request $request) {
+//     return view('welcome');
+// });
