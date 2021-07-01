f5f3dcae1a7a1d697644e1c7c133e8afaa94084c
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-07-01 15:42:00 +0600

    Fixed slide_logo_color bugs

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -340,12 +340,17 @@ class Thread extends Model
     public function get_brightness($hex) {
         // returns brightness value from 0 to 255
         // strip off any leading #
-        $hex = str_replace('#', '', $hex);
-        $c_r = hexdec(substr($hex, 0, 2));
-        $c_g = hexdec(substr($hex, 2, 2));
-        $c_b = hexdec(substr($hex, 4, 2));
+        try {
+            $hex = str_replace('#', '', $hex);
+            $c_r = hexdec(substr($hex, 0, 2));
+            $c_g = hexdec(substr($hex, 2, 2));
+            $c_b = hexdec(substr($hex, 4, 2));
 
-        return (($c_r * 299) + ($c_g * 587) + ($c_b * 114)) / 1000;
+            return (($c_r * 299) + ($c_g * 587) + ($c_b * 114)) / 1000;
+        } catch (\Throwable $th) {
+            return 0;
+        }
+
     }
 
     public function getSlideLogoColorAttribute(){
